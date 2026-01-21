9093c89d49d7b2201af6a2ae98bb2c97d04f9227
Jonas Rodrigues <313741+jxonas@users.noreply.github.com>
2026-01-21 22:18:34 +0000

    chore: prepare v2.0.1 release

--- src/aishell/cli.clj
+++ src/aishell/cli.clj
@@ -10,7 +10,7 @@
             [aishell.state :as state]
             [aishell.util :as util]))
 
-(def version "2.0.0")
+(def version "2.0.1")
 
 (defn print-version []
   (println (str "aishell " version)))
