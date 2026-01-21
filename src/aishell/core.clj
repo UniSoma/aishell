afdfd6fe32fae458a40ea4b6fc773dd9db13db36
Jonas Rodrigues <313741+jxonas@users.noreply.github.com>
2026-01-21 20:18:48 +0000

    refactor(18-01): simplify core.clj with static require
    - Use static (:require [aishell.cli :as cli]) at top level
    - Remove dynamic require hack and resolve call
    - Remove version functions (moved to cli.clj)
    - Enables uberscript namespace detection

--- src/aishell/core.clj
+++ src/aishell/core.clj
@@ -1,24 +1,14 @@
 (ns aishell.core
-  (:require [aishell.output :as output]))
-
-(def version "2.0.0")
-
-(defn print-version []
-  (println (str "aishell " version)))
-
-(defn print-version-json []
-  (println (str "{\"name\":\"aishell\",\"version\":\"" version "\"}")))
+  (:require [aishell.cli :as cli]
+            [aishell.output :as output]))
 
 (defn -main [& args]
   (try
-    ;; Require cli at runtime to avoid circular dependency
-    (require '[aishell.cli :as cli])
-    ((resolve 'aishell.cli/dispatch) args)
+    (cli/dispatch args)
     (catch clojure.lang.ExceptionInfo e
       (output/error (ex-message e)))
     (catch Exception e
       (output/error (str "Unexpected error: " (.getMessage e))))))
 
-;; Entry point for script execution
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
